2988efaa53b25081eca0cfd633687665c3db2a9c
dheeraj1056 <dhimandheeraj111@gmail.com>
2026-05-31 14:09:49 +0530

    Fix: restore relative class in Layout.jsx - fixes page layout

--- shoe-erp-frontend/src/components/layout/Layout.jsx
+++ shoe-erp-frontend/src/components/layout/Layout.jsx
@@ -12,7 +12,7 @@ export default function Layout() {
       <Sidebar />
       <div className="flex flex-col flex-1 min-w-0 overflow-hidden relative">
         <Topbar onToggleNotifications={() => setShowDrawer(!showDrawer)} />
-        <main className="flex-1 overflow-y-auto p-6">
+        <main className="flex-1 overflow-y-auto p-6 relative">
           <Outlet />
         </main>
         
